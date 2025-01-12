979dc6c01a22a7f87e351003858a89baadb92381
devesh85 <deveshk@gmail.com>
2025-01-12 00:31:07 -0500

    adding enumeration

--- src/rust_book/slices.rs
+++ src/rust_book/slices.rs
@@ -1,8 +1,8 @@
 pub fn first_word_size(s: &String) -> usize{
     let bytes = s.as_bytes();
 
-    for(i, item) in bytes.iter().enumerate(){
-        if *item == b' ' {
+    for(i, &item) in bytes.iter().enumerate(){
+        if item == b' ' {
             return i;
         }
     }
