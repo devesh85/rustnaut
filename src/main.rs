d431494e1f873e5a8d8ab21b98eaafc443aa1e75
devesh85 <deveshk@gmail.com>
2025-01-12 00:11:04 -0500

    ref and borrow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use crate::exercises::string_manipulation::reverse;
 pub mod exercises;
+use crate::rust_book::ref_and_borrow::borrow;
+pub mod rust_book;
 
 fn main() {
     let s = "Devesh Kishore";
     let rev_s = reverse(s);
-    print!("{rev_s}\n")
+    print!("{rev_s}\n");
+    borrow();
 }
---
+++ src/rust_book/ref_and_borrow.rs
@@ -0,0 +1,9 @@
+pub  fn borrow(){
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{s1}' is {len}");
+}
+
+fn calculate_length(s: &String) -> usize{
+    s.len()
+}
